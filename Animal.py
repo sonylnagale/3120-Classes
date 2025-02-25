7e82a93929dcaa9c537fc2aea4fdccc80e89f75c
Richard De Leon <richarddeleon@Richards-MacBook-Air-6.local>
2025-02-24 19:40:59 -0500

    Changes Richard

--- Animal.py
+++ Animal.py
@@ -1,8 +1,28 @@
 class Animal:
-    def __init__(self, name):
-        self.__name = name 
-        print("hello, I am", self.__name)
+    def __init__(self, type, talk, age, gender, diet, habitat): 
+        self.__type = type
+        self.__gender = gender
+        self.__talk = talk
+        self.__age = age
+        self.__diet = diet
+        self.__habitat = habitat
 
-    def talk(self):
-        print("hi")
 
+    def get_type (self):
+        print(f"This animal is a: {self.__type}")
+
+    def make_sound (self):
+        print(F"This animal makes the sound: {self.__talk}")
+
+    def get_gender (self):
+        print(f"This animal is a: {self.__gender}")
+    
+    def get_age (self):
+        print(f"This animal is {self.__age} years old")
+
+    def get_diet (self):
+        print(f"this animal is a: {self.__diet}")
+
+    def get_habitat(self):
+        print(f"This animal lives in a: {self.__habitat}")
+       
