b59c3a3a50072753d40dc1aeb0e4375764e275b8
Ayub Tursonzadah <ayubtursonzadah@gmail.com>
2025-02-24 19:38:33 -0500

    first tryyy

--- Animal.py
+++ Animal.py
@@ -1,14 +1,23 @@
 class Animal:
     def __init__(self, name):
         self.__name = name 
-        print("Hello, I am", self.__name)
+        print("hello, I am", self.__name)
 
     def talk(self):
-        print("Hi")
-
-    def walk(self):
-        print(f"{self.__name} is walking.")
-
+        print("WOOF!")
+#Addtional 5 functions
+    
     def eat(self, food):
-        print(f"{self.__name} is eating {food}.")
+        print(f"{self.__name} eats {food}.")  
+        
+    def sleep(self, hours):
+        print(f"{self.__name} sleeps for {hours} hours.")
 
+    def play(self):
+        print(f"{self.__name} is playing fetch!")
+
+    def groom(self):
+        print(f"{self.__name} is cleaning himself with water.")
+
+    def make_sound(self):
+        print(f"{self.__name} makes a barking sound.")
