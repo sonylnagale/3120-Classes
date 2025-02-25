0096e1ed6f6f27088fae9ca12240be99f43008fe
Tenzin Phuntsok <tenzinphun2004@gmail.com>
2025-02-24 19:24:09 -0500

    Tenzin P Push

--- Animal.py
+++ Animal.py
@@ -1,8 +1,22 @@
 class Animal:
-    def __init__(self, name):
-        self.__name = name 
-        print("hello, I am", self.__name)
+    def __init__(self, name, species, gender):
+        self.__name = name
+        self.__species = species
+        self.__gender = gender
+        print("hello I am", self.__name)
+
 
     def talk(self):
-        print("hi")
+        print(self.__name)
+
+    def print_species(self):
+        print("I am a",self.__species)
+
+    def print_gender(self):
+        print("I am a",self.__gender)
+
+    def excrete_waste(self):
+        print("I have excreted my waste")
+
+    
 
