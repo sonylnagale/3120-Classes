d119752ff8732dac1ecfe6dc8792567d53d409a6
Jason Maza <jason.j.maza@gmail.com>
2025-02-24 19:18:30 -0500

    uploading my changes

--- Animal.py
+++ Animal.py
@@ -2,7 +2,16 @@ class Animal:
     def __init__(self, name):
         self.__name = name 
         print("hello, I am", self.__name)
+       
 
     def talk(self):
         print("hi")
 
+    def fly (self):
+        print(self.__name, "can fly")
+
+    def swim(self):
+        print(self.__swim, "can swim")
+
+    def climb(self):
+        print(self.__climb, "is able to climb")
