a269fad8363f1709977ffd6ed50aa4add60cc75b
J-create01 <jiaxin.wu3@baruchmail.cuny.edu>
2025-02-24 19:19:35 -0500

    Updated

--- Animal.py
+++ Animal.py
@@ -1,8 +1,33 @@
 class Animal:
-    def __init__(self, name):
-        self.__name = name 
-        print("hello, I am", self.__name)
+    def __init__(self, name, eye, food, speed=5, favorite_animal=""):
+        self._name = name
+        self._eye_color = eye
+        self._food = food
+        self._speed = speed
+        self._favorite_animal = favorite_animal
+        print(f"Hello, I am {self._name}")
 
     def talk(self):
-        print("hi")
+        print("Hi")
+
+    def eat(self):
+        print(f"{self._name} has {self._eye_color} eyes and eats {self._food}, yum yum!")
+        print(f"Speed before eating: {self._speed}")
+        self._speed += 1  # Increase speed after eating
+        print(f"Speed after eating: {self._speed}")
+
+    def likes(self):
+        print(f"{self._name} likes {self._favorite_animal}!")
+
+    def eye_color(self):
+        return self._eye_color
+
+    def get_speed(self):
+        return self._speed
+
+    def set_speed(self, speed):
+        if speed >= 0:
+            self._speed = speed
+        else:
+            print("Speed cannot be negative.")
 
